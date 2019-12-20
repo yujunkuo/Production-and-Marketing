d6ec3d5b894e16628e07532dd75683c1c5e3dbe1
yujunkuo <johnboy880313@gmail.com>
2019-12-21 00:20:55 +0800

    KMeans Completed

--- marketing/views.py
+++ marketing/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .forms import CustomerForm
+from production.models import Member, Dish, Order
 
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.cluster import KMeans
+from datetime import datetime
+
+from io import BytesIO
+import base64
+
 # Create your views here.
 
 def main(request):
@@ -25,12 +34,45 @@ class CustomerView(TemplateView):
     def post(self, request):
         form = CustomerForm(request.POST, request.FILES)
         if form.is_valid():
-            output = self.handle_file(request.FILES['file'])
+            plot_res, mname_list, age_list, consumption_list, kmeans_fit = self.handle_file(request.FILES['file'])
             form = CustomerForm()
-        args = {'form': form, 'output': output}
+        args = {'form': form, "plot_res": plot_res, "mname_list": mname_list, "age_list": age_list, "consumption_list": consumption_list, "kmeans_fit": kmeans_fit}
         return render(request, self.template_name, args)
 
     def handle_file(self, file):
-        df = pd.DataFrame()
-        kmeans_fit = KMeans(n_clusters=3).fit_predict(df.iloc[1:,1:])
-        return kmeans_fit
+        memberid_list = []
+        mname_list = []
+        gender_list = []
+        age_list = []
+        pets_list = []
+        student_list = []
+        consumption_list = []
+        curr = datetime.now()
+        for cust in Member.objects.all():
+            memberid_list.append(cust.MemberID)
+            mname_list.append(cust.mName)
+            gender_list.append(cust.Gender)
+            # Calculate the member's age
+            age = (curr.year - cust.BDay.year) - ((curr.month, curr.day) < (cust.BDay.month, cust.BDay.day))
+            age_list.append(age)
+            # Calculate the member's total consumption amount
+            cons = 0
+            orders = Order.objects.filter(MemberID=cust.MemberID)
+            for order in orders:
+                dish = Dish.objects.filter(dName=order.dName)
+                dish_price = dish[0].dPrice
+                cons += dish_price
+            consumption_list.append(cons)
+            pets_list.append(cust.Pets)
+            student_list.append(cust.Student)
+        df = pd.DataFrame({"id": memberid_list, "name": mname_list, "age": age_list, "consumption": consumption_list})
+        model = KMeans(n_clusters=3)
+        kmeans_fit = model.fit_predict(df.iloc[:,2:])
+        for i in set(kmeans_fit):
+            plt.scatter(x=df[(model.labels_ == i)]["age"], y=df[(model.labels_ == i)]["consumption"],label=i)
+        plt.legend()
+        save_file = BytesIO()
+        plt.savefig(save_file, format='png')
+        plot_res = base64.b64encode(save_file.getvalue()).decode('utf8')
+        plt.close()
+        return plot_res, mname_list, age_list, consumption_list, kmeans_fit
