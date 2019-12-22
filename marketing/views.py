331ff0ae7b57b44a90d3dc2e21b5ce7a894fee12
yujunkuo <johnboy880313@gmail.com>
2019-12-23 01:54:07 +0800

    decision tree has done, and the member's database has also been updated

--- marketing/views.py
+++ marketing/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
 from .forms import CustomerForm
 from production.models import Member, Dish, Order
 
+# Kmeans packages
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from datetime import datetime
+
+# Decision Tree packages
+from sklearn import tree
+import graphviz
+import pydotplus
+from sklearn.externals.six import StringIO
 
 from io import BytesIO
 import base64
@@ -29,6 +37,7 @@ def swot(request):
 def stp(request):
     return render(request, 'stp.html')
 
+
 class KmeansView(TemplateView):
     template_name = 'customerAnalysis.html'
 
@@ -50,16 +59,12 @@ class KmeansView(TemplateView):
     def handle_file(self, file):
         memberid_list = []
         mname_list = []
-        gender_list = []
         age_list = []
-        pets_list = []
-        student_list = []
         consumption_list = []
         curr = datetime.now()
         for cust in Member.objects.all():
             memberid_list.append(cust.MemberID)
             mname_list.append(cust.mName)
-            gender_list.append(cust.Gender)
             # Calculate the member's age
             age = (curr.year - cust.BDay.year) - ((curr.month, curr.day) < (cust.BDay.month, cust.BDay.day))
             age_list.append(age)
@@ -71,8 +76,6 @@ class KmeansView(TemplateView):
                 dish_price = dish[0].dPrice
                 cons += dish_price
             consumption_list.append(cons)
-            pets_list.append(cust.Pets)
-            student_list.append(cust.Student)
         df = pd.DataFrame({"id": memberid_list, "name": mname_list, "age": age_list, "consumption": consumption_list})
         model = KMeans(n_clusters=3)
         kmeans_fit = model.fit_predict(df.iloc[:, 2:])
@@ -89,4 +92,45 @@ class KmeansView(TemplateView):
 class DecisionTreeView(TemplateView):
 
     def get(self, request):
-        return render(request, "decisionTree.html")
+        pets_list = [1 if each.Pets else 0 for each in Member.objects.all()]
+        student_list = [1 if each.Student else 0 for each in Member.objects.all()]
+        gender_list = [1 if each.Gender == "Male" else 0 for each in Member.objects.all()]
+        consumption_list = []
+        for cust in Member.objects.all():
+            # Calculate the member's total consumption amount
+            cons = 0
+            orders = Order.objects.filter(MemberID=cust.MemberID)
+            for order in orders:
+                dish = Dish.objects.filter(dName=order.dName)
+                dish_price = dish[0].dPrice
+                cons += dish_price
+            consumption_list.append(cons)
+        mean = sum(consumption_list) / len(consumption_list)
+        consumption_list = ["High" if each >= mean else "Low" for each in consumption_list]
+        df = pd.DataFrame({
+            "pets": pets_list,
+            "student": student_list,
+            "gender": gender_list,
+            "consumption": consumption_list
+        })
+        clf = tree.DecisionTreeClassifier()
+        clf = clf.fit(df[["pets", "student", "gender"]], df["consumption"])
+        test_df = pd.DataFrame({
+            "pets": [0, 0, 0, 0, 1, 1, 1, 1],
+            "student": [0, 0, 1, 1, 0, 0, 1, 1],
+            "gender": [0, 1, 0, 1, 0, 1, 0, 1]
+        })
+        pre = clf.predict(test_df)
+        return render(request, "decisionTree.html", {
+            "pre0": pre[0],
+            "pre1": pre[1],
+            "pre2": pre[2],
+            "pre3": pre[3],
+            "pre4": pre[4],
+            "pre5": pre[5],
+            "pre6": pre[6],
+            "pre7": pre[7]
+        })
+
+
+
--- production/models.py
+++ production/models.py
@@ -1,54 +1,57 @@
 from django.db import models
 
+
 # Create your models here.
 class Member(models.Model):
-    MemberID = models.PositiveIntegerField(primary_key = True)
-    mName = models.CharField(max_length = 20)
-    Gender = models.CharField(max_length = 10)
-    Phone = models.CharField(max_length = 13)
-    Email = models.EmailField(max_length = 100, blank = True)
-    BDay = models.DateField(null = False)
-    Pets = models.BooleanField
-    Student = models.BooleanField
+    MemberID = models.PositiveIntegerField(primary_key=True)
+    mName = models.CharField(max_length=20)
+    Gender = models.CharField(max_length=10)
+    Phone = models.CharField(max_length=13)
+    Email = models.EmailField(max_length=100, blank=True)
+    BDay = models.DateField(null=False)
+    Pets = models.BooleanField(default=True)
+    Student = models.BooleanField(default=True)
 
     def __str__(self):
         return self.mName
 
 
 class Dish(models.Model):
-    dName = models.CharField(max_length = 50, primary_key = True)
+    dName = models.CharField(max_length=50, primary_key=True)
     dPrice = models.PositiveIntegerField()
     Mid = models.ManyToManyField(Member, through='Order')
 
     def __str__(self):
         return self.dName
 
+
 class Firm(models.Model):
-    FirmID = models.PositiveIntegerField(primary_key = True)
-    fName = models.CharField(max_length = 20)
-    Tele = models.CharField(max_length = 10)
-    Address = models.CharField(max_length = 50)
+    FirmID = models.PositiveIntegerField(primary_key=True)
+    fName = models.CharField(max_length=20)
+    Tele = models.CharField(max_length=10)
+    Address = models.CharField(max_length=50)
 
     def __str__(self):
         return self.fName
 
+
 class Stock(models.Model):
-    sName = models.CharField(max_length = 50, primary_key = True)
+    sName = models.CharField(max_length=50, primary_key=True)
     sNum = models.PositiveIntegerField()
     sPrice = models.PositiveIntegerField()
     Expired = models.DateField()
     dish = models.ManyToManyField(Dish, through='Made')
     firm = models.ManyToManyField(Firm, through='ProvideStock')
 
-    class Meta():
-        unique_together = (('sName', 'Expired'))
+    class Meta:
+        unique_together = ('sName', 'Expired')
 
     def __str__(self):
         return self.sName
 
 
 class Equipment(models.Model):
-    eName = models.CharField(max_length = 50, primary_key = True)
+    eName = models.CharField(max_length=50, primary_key=True)
     eNum = models.PositiveIntegerField()
     ePrice = models.PositiveIntegerField()
     firm = models.ManyToManyField(Firm, through='ProvideEquip')
@@ -56,34 +59,42 @@ class Equipment(models.Model):
     def __str__(self):
         return self.eName
 
+
 class Order(models.Model):
-    oTime = models.DateTimeField(auto_now_add = 'True', primary_key = True)
+    oTime = models.DateTimeField(auto_now_add='True', primary_key=True)
     MemberID = models.ForeignKey(Member, on_delete=models.CASCADE)
     dName = models.ForeignKey(Dish, on_delete=models.CASCADE)
     oNum = models.PositiveIntegerField()
-    class Meta: 
-        unique_together = (("oTime","MemberID", "dName"))
+
+    class Meta:
+        unique_together = ("oTime", "MemberID", "dName")
+
 
 class Made(models.Model):
-    mTime = models.DateTimeField(auto_now_add = 'True', primary_key = True)
+    mTime = models.DateTimeField(auto_now_add='True', primary_key=True)
     mDish = models.ForeignKey(Dish, on_delete=models.CASCADE)
     mStock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     mNum = models.PositiveIntegerField()
-    class Meta: 
-        unique_together = (("mTime","mDish", "mStock"))
+
+    class Meta:
+        unique_together = ("mTime", "mDish", "mStock")
+
 
 class ProvideStock(models.Model):
-    psTime = models.DateTimeField(auto_now_add = 'True', primary_key = True)
+    psTime = models.DateTimeField(auto_now_add='True', primary_key=True)
     psFirm = models.ForeignKey(Firm, on_delete=models.CASCADE)
     pStock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     psNum = models.PositiveIntegerField()
-    class Meta: 
-        unique_together = (("psTime","pStock", "psNum"))
+
+    class Meta:
+        unique_together = ("psTime", "pStock", "psNum")
+
 
 class ProvideEquip(models.Model):
-    peTime = models.DateTimeField(auto_now_add = 'True', primary_key = True)
+    peTime = models.DateTimeField(auto_now_add='True', primary_key=True)
     peFirm = models.ForeignKey(Firm, on_delete=models.CASCADE)
     pEquip = models.ForeignKey(Equipment, on_delete=models.CASCADE)
     peNum = models.PositiveIntegerField()
-    class Meta: 
-        unique_together = (("peTime","pEquip", "peNum"))
+
+    class Meta:
+        unique_together = ("peTime", "pEquip", "peNum")
